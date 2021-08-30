b314de9be677c04e2881f3c9e8250429247d9bec
MiHyun Park <69749222+mihyunP@users.noreply.github.com>
2021-08-30 23:02:32 +0900

    2021 카카오 채용연계형 인턴십

---
+++ Programmers/level1/숫자 문자열과 영단어.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        String tmpS =s;
+       
+        // 1. (String) 영단어 -> (String) 숫자 바꾸기
+        while(true){
+        tmpS = s.replace("zero","0")
+            .replace("one","1")
+            .replace("two","2")
+            .replace("three","3")
+            .replace("four","4")
+            .replace("five","5")
+            .replace("six","6")
+            .replace("seven","7")
+            .replace("eight","8")
+            .replace("nine","9");
+        
+        s = tmpS;
+        if(s.equals(tmpS)){
+            // System.out.println(s);
+            break;
+        }
+            
+        }  
+        // 2. (String) 숫자 -> (int) 숫자
+        answer = Integer.parseInt(s);
+        
+        // for(int i=0; i<s.length();i++){
+        //     int tmpAns = (s.charAt(i)-'0'); // 문자형으로 선언된 숫자를 int형으로 바꾸기
+        //     // System.out.println(tmpAns);
+        //     for(int j=s.length()-1-i; j>0;j--){
+        //         tmpAns*=10;
+        //     }
+        //      answer += tmpAns;
+        // }
+               
+        return answer;
+    }
+}
