5d06e25858bed4761034e75f946467b5b8e17d2b
MiHyun Park <69749222+mihyunP@users.noreply.github.com>
2021-09-02 22:19:32 +0900

    연습문제

--- Programmers/level1/수박수박수박수박수박수?.java
+++ Programmers/level1/수박수박수박수박수박수?.java
@@ -4,9 +4,11 @@ class Solution {
         
         for(int i=0; i<n;i++){
             if(i%2==0){
-                answer+="수";
+                // answer+="수";
+                answer = answer.concat("수");
             }else{
-                answer+="박";
+                // answer+="박";
+                answer = answer.concat("박");
             }
         }
         
@@ -14,4 +16,4 @@ class Solution {
     }
 }
 
-// 문자열 만들기 : +로 한 글자씩 더해서 만들수 있음
+// '수박수박수...'문자열 만들기 : +로 한 글자씩 더해서 만들수 있음, concat()함수 사용해서 한글자씩 더해서 만들 수 있음
