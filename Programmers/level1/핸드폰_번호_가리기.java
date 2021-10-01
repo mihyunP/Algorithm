999418becebbe2bf46a7bdfacf3175c337492b61
MiHyun Park <69749222+mihyunP@users.noreply.github.com>
2021-10-01 23:14:26 +0900

    연습문제

---
+++ Programmers/level1/핸드폰_번호_가리기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String phone_number) {
+        int len = phone_number.length();
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<len-4; i++){
+            sb.append("*");
+        }
+        sb.append(phone_number.substring(len-4,len));
+        
+        String answer = new String(sb);
+        return answer;
+    }
+}
