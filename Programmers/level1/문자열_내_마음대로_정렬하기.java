d2a72152cc0edc51cb7ad030bffba54d7909c796
MiHyun Park <69749222+mihyunP@users.noreply.github.com>
2021-10-03 15:12:27 +0900

    연습문제

--- Programmers/level1/문자열_내_마음대로_정렬하기.java
+++ Programmers/level1/문자열_내_마음대로_정렬하기.java
@@ -23,3 +23,15 @@ class Solution {
 
 // Arrays.sort() vs Collections.sort() 차이점 <- 배열 정렬할 때,ArrayList등 정렬할 때 씀
 // sort(정렬 대상, 정렬 기준) <- 정렬 기준은 default가 오름차순 (생략가능)
+// implements 안해도 Comparator 쓸 수 있는지?
+// --------------------------------------
+// Comparator 따로 쓰는 경우 : 
+// Comparator<String> comp = new Comparator<String>() {
+//         @Override
+//         public int compare(String s1, String s2) {
+//             return s1.charAt(n) - s2.charAt(n);
+//         }
+//     };
+        
+//     // n번째 인덱스의 문자로 sorting (오름차순)
+//     Arrays.sort(strings, comp);
